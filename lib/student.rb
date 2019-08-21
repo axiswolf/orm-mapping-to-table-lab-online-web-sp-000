d4ad5efa123458f372961bbfafd05316211e6b5a
Karen V <karenvo@usc.edu>
2019-08-21 01:02:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  attr_accessor :id, :name, :grade
+  attr_accessor :name, :grade
   def initialize(id = nil, name, grade)
     @name = name
     @grade = grade
