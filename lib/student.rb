49608eace1124a1da8bfba7117b5a3cae79abab0
Karen V <karenvo@usc.edu>
2019-08-21 01:05:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,11 @@ class Student
   end
   def save
     # saves an instance of the Student class to the database
+    sql = <<-SQL 
+      INSERT INTO students(name, grade)
+      VALUES(?,?)
+      SQL
+      DB[:conn].execute(sql,self.name, self.grade)
   end
   def self.create_table
     # creates the students table in the database
