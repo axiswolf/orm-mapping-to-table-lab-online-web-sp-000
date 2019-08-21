a872b688ec8237c77738604d24bfbf0e19874d69
Karen V <karenvo@usc.edu>
2019-08-21 01:01:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,23 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
+  attr_accessor :id, :name, :grade
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    # creates the students table in the database
+  end
+
+  def self.drop_table
+    # drops the students table form the database
+  end
+
+  def self.create
+    # returns the new object that it instantiated
+  end
+
 end
